fcc2febec4fa877a0428c1f13b80269d1b5e103c
Farida Pangaribuan <faridapangaribuan24@gmail.com>
2024-09-09 20:52:47 +0700

    Js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function transformArray(arr) {
-    return arr.map(num => arr.reduce((acc,val) => acc + val, 0) - num);
+function reverseAndTimeArray(arr) {
+ 
+    if (arr.length === 0) return [];
+    
+
+    let reversedArr = arr.slice().reverse();
+
+
+    let resultArr = reversedArr.map((_, i) => 
+        reversedArr.reduce((sum, value, index) => 
+            sum + (index <= i ? value : 0), 0)
+    );
+    
+    return resultArr;
 }
 
-const input = [1,4,6,7];
-const output = transformArray(input);
-console.log(output);
+console.log(reverseAndTimeArray([1, 4, 6, 7]));
+console.log(reverseAndTimeArray([7, 4, 2, 1])); 
+console.log(reverseAndTimeArray([5]));          
+console.log(reverseAndTimeArray([1, 3]));       
